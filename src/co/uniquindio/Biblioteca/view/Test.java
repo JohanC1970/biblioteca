98b64327fa35758706344d1cec2358a17bf12399
johanC1970 <johanc.garciag@uqvirtual.edu.co>
2023-03-28 09:52:50 -0500

    Se agrega el mensaje en el main

--- src/co/uniquindio/Biblioteca/view/Test.java
+++ src/co/uniquindio/Biblioteca/view/Test.java
@@ -3,12 +3,6 @@ package co.uniquindio.Biblioteca.view;
 public class Test {
 
 	public static void main(String args[]){
-		boolean a = false;
-		boolean b = false;
-		boolean c = false;
-		if(a && b){
-			System.out.println(true);
-		}
-		System.out.println(c);
+		System.out.println("Este es un mensaje nuevo");
 	}
 }
